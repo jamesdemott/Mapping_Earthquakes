608309c4019f29ab6c46b276f76d19283c383356
jamesdemott <jamesandersondemott@gmail.com>
2020-10-27 14:59:33 -0700

    finished adding layers, geojson

--- static/js/logic.js
+++ static/js/logic.js
@@ -55,13 +55,18 @@ let sanFranAirport =
 // }).addTo(map);
 
 
-let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/jamesdemott/Mapping_Earthquakes/Mapping_GeoJSON_Points/static/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+  L.geoJson(data, {
+      onEachFeature: function(feature, layer){
+          console.log(layer);
+          layer.bindPopup("<h1>" + feature.properties.name + "</h1>")
+      }
+  }).addTo(map);
 });
 
 // We create the tile layer that will be the background of our map.
@@ -73,5 +78,21 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+  };
+  
+L.control.layers(baseMaps).addTo(map);
 
 
