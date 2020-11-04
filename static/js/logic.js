17defb84c007c33f9d0bf52384794888c585dc9f
jamesdemott <jamesandersondemott@gmail.com>
2020-11-04 11:22:25 -0800

    works

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,6 +4,8 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([30, 30], 2);
 
+API_KEY = "pk.eyJ1IjoiaGV5ZGVtbyIsImEiOiJja2gzczRzYXYwMWE2MnlwNG91Zm42MW4xIn0.6B_vzs1Ji1enb4_R9IJgxQ"
+
 // // Get data from cities.js
 // let cityData = cities;
 
